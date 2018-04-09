907065a7175c2afa8a4cb3fca6db59eba4e8e1ac
Carsten Klein <trancesilken@gmail.com>
2018-04-09 20:10:19 +0200

    fix #62: also run tests inherited from [abstract] base classes fix also existing tests and provide for a facility that allows new tests to be implemented in a shorter time span existing test failures are unrelated and relate to @types/mocha now declaring entities that are redeclared by this

---
+++ tests/ts/suite.inheritance.suite.ts
@@ -0,0 +1,34 @@
+import { suite, test } from "../../index";
+import { assert } from "chai";
+
+@suite class PartialTest {
+
+  protected _truthyValue: boolean;
+
+  before() {
+
+    this._truthyValue = true;
+  }
+
+  @test "test from PartialTest"() {
+
+    assert.isTrue(this._truthyValue, "truthyValue should have been true");
+  }
+}
+
+@suite class CompleteTest extends PartialTest {
+
+  private _falsyValue = true;
+
+  before(): void {
+
+    super.before();
+
+    this._falsyValue = false;
+  }
+
+  @test "test from CompleteTest"() {
+
+    assert.isFalse(this._falsyValue, "falsyValue should have been false");
+  }
+}

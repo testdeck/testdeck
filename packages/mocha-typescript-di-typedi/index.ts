dccaf195143c95a7ac4bbf3258ebcb0ae06fc9ec
Carsten Klein <trancesilken@gmail.com>
2019-02-01 21:58:05 +0100

    feature(#157): use webpack

--- packages/mocha-typescript-di-typedi/index.ts
+++ packages/mocha-typescript-di-typedi/index.ts
@@ -5,6 +5,6 @@ import { Container } from "typedi";
 import { registerDI } from "mocha-typescript";
 
 registerDI({
-    handles: (cls) => Container.has(cls),
+    handles: (cls) => true,
     create: (cls) => Container.get(cls),
 });
---
+++ packages/mocha-typescript/webpack.config.js
@@ -0,0 +1,30 @@
+var path = require('path');
+var nodeModulesPath = path.resolve(__dirname, 'node_modules');
+var distPath = path.resolve(__dirname, 'dist');
+var webpack = require('webpack');
+
+module.exports = {
+  mode: 'production',
+  entry: 'index.ts',
+  output: {
+    path: distPath,
+    filename: 'index.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        loader: 'ts-loader'
+      }
+    ]
+  },
+  resolve: {
+    modules: [
+      __dirname,
+      'node_modules'
+    ],
+    extensions: [
+      '.ts'
+    ]
+  }
+};

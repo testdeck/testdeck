75c872194a066d4ef176cb0d41922ba451617e7c
PanayotCankov <panayot.cankov@telerik.com>
2019-03-13 23:26:22 +0200

    Add test coverage for all decorator overloads

--- packages/@testdeck/core/test.ts
+++ packages/@testdeck/core/test.ts
@@ -68,7 +68,7 @@ module LoggingClassTestUI {
     }
 }
 
-describe("Decorators", function() {
+describe("decorators", function() {
 
     let ui: LoggingClassTestUI;
     beforeEach("create ui", function() {
@@ -78,7 +78,10 @@ describe("Decorators", function() {
         ui = null;
     });
 
-    it("Just @suite and @test", function() {
+    // TODO: `suite("s", () => { it("t", () => {})});`
+    it("functional usage");
+
+    it("plain @suite and @test", function() {
 
         @ui.suite class SimpleSuite {
             @ui.test simpleTest() {}
@@ -92,5 +95,114 @@ describe("Decorators", function() {
                 "name": "simpleTest",
             }]
         }]);
+
+    });
+
+    it("execution modifier and settings combo", function() {
+
+        @ui.suite(ui.slow(10), ui.timeout(20), ui.retries(3))
+        class SuiteWithTimeouts {
+            @ui.test(ui.slow(20), ui.timeout(40), ui.retries(5))
+            test1() {}
+            @ui.test
+            test2() {}
+            @ui.test(ui.skip)
+            test3() {}
+        }
+
+        @ui.suite
+        @ui.slow(10)
+        @ui.timeout(20)
+        @ui.retries(3)
+        class SuiteWithTimeoutsAsDecorators {
+            @ui.test
+            @ui.slow(5)
+            @ui.skip
+            test1() {}
+
+            @ui.test(ui.timeout(10))
+            @ui.skip(true)
+            test2() {}
+
+            @ui.test(ui.retries(2))
+            @ui.skip(false)
+            test3() {}
+        }
+
+        @ui.suite
+        @ui.only
+        class SuiteOnlyThis {
+            @ui.test
+            test1() {}
+        }
+
+        assert.deepEqual(ui.root, [{
+            "type": "suite",
+            "name": "SuiteWithTimeouts",
+            "settings": {
+                "slow": 10,
+                "timeout": 20,
+                "retries": 3
+            },
+            "children": [{
+                "type": "test",
+                "name": "test1",
+                "settings": {
+                    "slow": 20,
+                    "timeout": 40,
+                    "retries": 5
+                }
+            }, {
+                "type": "test",
+                "name": "test2"
+            }, {
+                "type": "test",
+                "name": "test3",
+                "settings": {
+                    "execution": "skip"
+                }
+            }]
+        }, {
+            "type": "suite",
+            "name": "SuiteWithTimeoutsAsDecorators",
+            "settings": {
+                "slow": 10,
+                "timeout": 20,
+                "retries": 3
+            },
+            "children": [{
+                "type": "test",
+                "name": "test1",
+                "settings": {
+                    "slow": 5,
+                    "execution": "skip"
+                }
+            }, {
+                "type": "test",
+                "name": "test2",
+                "settings": {
+                    "timeout": 10,
+                    "execution": "skip"
+                }
+            }, {
+                "type": "test",
+                "name": "test3",
+                "settings": {
+                    "retries": 2
+                }
+            }]
+        }, {
+            "type": "suite",
+            "name": "SuiteOnlyThis",
+            "settings": {
+                "execution": "only"
+            },
+            "children": [{
+                "type": "test",
+                "name": "test1"
+            }]
+        }]);
     });
 });
+
+declare var console;

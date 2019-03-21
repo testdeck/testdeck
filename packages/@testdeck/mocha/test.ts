918a218751368ab584e054a5b66b0cec8060e462
PanayotCankov <panayot.cankov@telerik.com>
2019-03-21 22:57:42 +0200

    Add some unit tests for mocha

--- packages/@testdeck/mocha/test.ts
+++ packages/@testdeck/mocha/test.ts
@@ -1,74 +1,130 @@
-import { suite, test, timeout, slow, retries } from "./index";
-import { METHODS } from "http";
+import { assert } from "chai";
+import { retries, slow, suite, test, timeout } from "./index";
 
 describe("tests", function() {
 
+    let events: string[];
+
     @suite class Suite {
-        static before() {
+        public static before() {
+            events.push("Suite statc before");
         }
 
-        before() {
+        public before() {
+            events.push("Suite before");
         }
 
         @test(timeout(1000), slow(500), retries(3))
-        test() {
+        public test() {
+            events.push("Suite test");
         }
 
-        @test.pending(timeout(1000)) pendingTest() {
+        @test.pending(timeout(1000)) public pendingTest() {
+            events.push("Suite pendingTest");
         }
 
-        @test.skip(slow(500)) skippedTest() {
+        @test.skip(slow(500)) public skippedTest() {
+            events.push("Suite skippedTest");
         }
 
-        after() {
+        public after() {
+            events.push("Suite after");
         }
 
-        static after() {
+        public static after() {
+            events.push("Suite statc after");
         }
     }
 
     @suite class CallbacksSuite {
-        static before(done) {
-            done();
+        public static before(done) {
+            events.push("CallbacksSuite statc before");
+            setTimeout(done, 0);
         }
 
-        before(done) {
-            done();
+        public before(done) {
+            events.push("CallbacksSuite before");
+            setTimeout(done, 0);
         }
 
-        
         @test(timeout(1000), slow(500), retries(3))
-        test(done) {
-            done();
+        public test(done) {
+            events.push("CallbacksSuite test");
+            setTimeout(done, 0);
         }
 
-        @test.pending(timeout(1000)) pendingTest(done) {
-            done();
+        @test
+        @timeout(100)
+        public test2(done) {
+            events.push("CallbacksSuite test2");
+            setTimeout(done, 1);
         }
 
-        @test.skip(slow(500)) skippedTest(done) {
-            done();
+        @test
+        @retries(100)
+        public test3(done) {
+            events.push("CallbacksSuite test3");
+            setTimeout(done, 1);
         }
 
-        after(done) {
-            done();
+        @test.pending(timeout(1000))
+        public pendingTest(done) {
+            events.push("CallbacksSuite pendingTest");
+            setTimeout(done, 0);
         }
 
-        static after(done) {
-            done();
+        @test.skip(slow(500))
+        public skippedTest(done) {
+            events.push("CallbacksSuite skippedTest");
+            setTimeout(done, 0);
+        }
+
+        public after(done) {
+            events.push("CallbacksSuite after");
+            setTimeout(done, 0);
+        }
+
+        public static after(done) {
+            events.push("CallbacksSuite static after");
+            setTimeout(done, 0);
         }
     }
 
     @suite.pending class PendintSuite {
-        @test test() {
+        @test public test() {
+            events.push("PendintSuite test");
         }
     }
 
     @suite.skip class SkippedSuite {
-        @test test() {
+        @test public test() {
+            events.push("SkippedSuite test");
         }
     }
 
-    it("log", function() {
+    before(function() {
+        events = [];
+    });
+
+    after(function() {
+        assert.deepEqual(events, [
+            "Suite statc before",
+            "Suite before",
+            "Suite test",
+            "Suite after",
+            "Suite statc after",
+            "CallbacksSuite statc before",
+            "CallbacksSuite before",
+            "CallbacksSuite test",
+            "CallbacksSuite after",
+            "CallbacksSuite before",
+            "CallbacksSuite test2",
+            "CallbacksSuite after",
+            "CallbacksSuite before",
+            "CallbacksSuite test3",
+            "CallbacksSuite after",
+            "CallbacksSuite static after"
+        ]);
+        events = undefined;
     });
 });

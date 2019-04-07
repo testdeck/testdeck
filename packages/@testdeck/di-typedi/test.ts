5d4cd2d747495e16d97bc486c8b254e2c8189ec5
Carsten Klein <trancesilken@gmail.com>
2019-04-07 16:12:39 +0200

    chore #222: add missing tests to di-typedi

---
+++ packages/@testdeck/di-typedi/test.ts
@@ -0,0 +1,30 @@
+import { suite, test } from "@testdeck/mocha";
+import { expect } from "chai";
+
+// register TypeDI support with testdeck and also let reflect-metadata do it's magic
+import "./index";
+import "reflect-metadata";
+
+import { Service } from "typedi";
+
+@Service()
+class SomeService {
+
+  public doSomething(): boolean {
+
+    return true;
+  }
+}
+
+@suite
+class Hello {
+
+  constructor(private service: SomeService) {
+  }
+
+  @test
+  world() {
+
+    expect(this.service.doSomething()).to.be.true;
+  }
+}

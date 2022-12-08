f30dd58862042cb59ed3c8cc3d168da9383a806d
Carsten Klein <trancesilken@gmail.com>
2022-12-09 00:32:31 +0100

    Add license header to all sources fix #379

--- packages/mocha/test.ts
+++ packages/mocha/test.ts
@@ -1,3 +1,17 @@
+// Copyright 2016-2022 Testdeck Team and Contributors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 import { assert } from "chai";
 import { context, retries, slow, suite, test, timeout } from "./index";
 
@@ -105,12 +119,12 @@ describe("tests", function() {
     @suite class SuiteContext {
 
         static async before() {
-            this[context].timeout(50);
+            assert.isDefined(this[context]);
             events.push("Has mocha before all suite context");
         }
 
         before() {
-            this[context].timeout(50);
+            assert.isDefined(this[context]);
             events.push("Has mocha before each suite context");
         }
 
